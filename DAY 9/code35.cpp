7334e8f4df1ef63c1cb53ee90db6a9a1260d595d
Anurag-2309 <anuragyadavanu28@gmail.com>
2026-07-01 01:21:37 +0530

    DAY 9 HOMEWORK

---
+++ DAY 9/code35.cpp
@@ -0,0 +1,17 @@
+// Q35. Write a program to Print repeated character pattern.
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        char ch = 'A' + i - 1;
+        for (int j = 1; j <= i; j++)
+            cout << ch;
+        cout << endl;
+    }
+
+    return 0;
+}
