399820004fb66f1df659c98168ab397085523aa5
Anurag-2309 <anuragyadavanu28@gmail.com>
2026-07-01 01:20:07 +0530

    DAY 6 HOMEWORK

---
+++ DAY 6/Code22.cpp
@@ -0,0 +1,19 @@
+// Q22. Write a program to Convert binary to decimal.
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int binary, decimal = 0, base = 1, rem;
+    cin >> binary;
+
+    while (binary > 0) {
+        rem = binary % 10;
+        decimal += rem * base;
+        base *= 2;
+        binary /= 10;
+    }
+
+    cout << decimal;
+    return 0;
+}
